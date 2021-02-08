b4e8e03b0158f4899c186e4aabecb9ff397afba8
Arhan-55 <75885658+Arhan-55@users.noreply.github.com>
2021-02-08 18:39:53 +0530

    Add files via upload

---
+++ JS/Slingshot.js
@@ -0,0 +1,39 @@
+class slingshot{
+	constructor(body,anchor)
+	{
+		
+		var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.010, 
+			length:1
+			
+		}
+		
+		this.bodyA=body
+		this.pointB=anchor
+		this.slingshot = Constraint.create(options)
+		World.add(world,this.slingshot)
+	}
+
+	attach(body){
+		this.slingshot.bodyA=body;
+	}
+
+	fly()
+	{
+		this.slingshot.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.slingshot.bodyA)
+		{
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+
+			strokeWeight(4);
+			stroke("blue")		
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}
+	}
+}
